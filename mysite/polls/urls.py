fab8720d9962794be6aad2d62eaf5ff6af22d6f6
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-11-26 20:43:48 +0900

    part 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,10 +6,11 @@ from . import views
 #url 이름공간(namespace) 추가
 app_name = 'polls'
 urlpatterns = [
+  # question_id -> pk
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
